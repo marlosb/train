3f8c4a9f0506790cb16aae5dcddc85691baba542
Marlos Bosso <marlosbosso@outlook.com>
2022-09-01 20:17:51 -0300

    split main.py code into modules abt.py and train.py

---
+++ abt.py
@@ -0,0 +1,41 @@
+import datetime
+from dateutil.relativedelta import relativedelta
+
+import sqlalchemy
+from tqdm import tqdm
+
+def create_abt(QUERY_PATH, DATA_PATH, primeira_safra, ultima_safra):
+
+    with open( QUERY_PATH, 'r' ) as open_file:
+        query = open_file.read()
+
+    primeira_safra_datetime = (
+        datetime.datetime.strptime(primeira_safra, "%Y-%m-%d"))
+    ultima_safra_datetime = (
+        datetime.datetime.strptime(ultima_safra, "%Y-%m-%d"))
+
+    con = sqlalchemy.create_engine( 'sqlite:///' + DATA_PATH )
+
+    def exec_etl(queries, con):
+        for q in tqdm(queries.split(";")[:-1]):
+            con.execute(q)
+
+    while primeira_safra_datetime <= ultima_safra_datetime:
+        safra = primeira_safra_datetime.strftime("%Y-%m-%d")
+        query_exec = query.format( data_ref=safra )
+        primeira_safra_datetime = (primeira_safra_datetime
+                                    + relativedelta(months=+1))
+        
+        if safra == primeira_safra:
+            query_exec += f'''DROP TABLE IF EXISTS TB_ABT;
+                                CREATE TABLE TB_ABT AS
+                                SELECT '{safra}' as dt_ref,
+                                *
+                                FROM vw_olist_abt_p2;'''
+        else:
+            query_exec += f'''INSERT INTO TB_ABT
+                                SELECT '{safra}' as dt_ref,
+                                *
+                                FROM vw_olist_abt_p2;'''
+        print(safra)
+        exec_etl(query_exec, con)
